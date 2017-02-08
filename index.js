d61935cd578cd29c63c1da432ab4b903110fb87e
Mustapha <mustapha.o.ojo@student.uncommonhs.org>
2017-02-08 22:25:33 +0000

    I changed stuff

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 $(document).ready( function(){
     $('#change-color').click(function(){
         $('body').css("background-color","red");
-    })
-})
+    
+    });
 
+$("#submit-name").click(function() { var name = $("#name").val()
+$("#greeting").html("HI, " + $("#name").val() );
+    });
+});
